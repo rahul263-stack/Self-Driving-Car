e34651f7e61627ac5e4a5c063de9bf0c21fe3941
rahul263-stack <53581155+rahul263-stack@users.noreply.github.com>
2019-12-13 22:43:16 +0530

    Update hello world.py

--- Tenser Flow/hello world.py
+++ Tenser Flow/hello world.py
@@ -3,7 +3,7 @@ tf.disable_v2_behavior()
 
 # Create TensorFlow object called tensor
 hello_constant = tf.constant('Hello World!')
-
+# Print hello world in TensorFlow 
 with tf.Session() as sess:
     # Run the tf.constant operation in the session
     output = sess.run(hello_constant)
